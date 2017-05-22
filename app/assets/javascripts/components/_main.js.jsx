e1f604d199a472e2ae602c1406e120b60ae02775
Kirk Yuan <kirkyuan@cmail.carleton.ca>
2017-05-22 16:18:30 -0600

    Bootstrap working

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -1,10 +1,20 @@
-var Main = React.createClass({ 
-  render() { 
-    return ( 
-      <div> 
-        <h1>Hello, World!</h1> 
+var Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
         <LoginForm />
-      </div>  
-    )  
-  }  
+      </div>
+    )
+  }
+});
+
+var NavBar = React.createClass({
+  render(){
+    return(
+      <div className = "text-center">
+          <h1> Hi </h1>
+      </div>
+    )
+  }
 });
